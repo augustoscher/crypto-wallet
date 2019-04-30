dda050e8f4d1f1023b75c9a35b54ddd845b2edd1
augusto.scher <augusto.scher@senior.com.br>
2019-04-30 13:02:33 -0300

    mais sobre rotas

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,15 @@
 Rails.application.routes.draw do
   get 'welcome/index'
+  #como o nome da rota e diferente do path, tem que usar o 'to:'
+  get '/inicio', to: 'welcome#index'
+  #cria todas as rotas válidas para coin. cria as 7 rotas default para o model.
   resources :coins
+  #é possivel criar na mão
+  #get '/coins', to 'coins#index'
+   
+  #indica a rota root
   root to: 'welcome#index'
+  
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
