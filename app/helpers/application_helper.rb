a4b82a8b2b1b1b0c063ea9a759d046596cade1c3
augusto.scher <augusto.scher@senior.com.br>
2019-04-30 12:43:31 -0300

    helpers customizados

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+    #helper customizado para toda a aplicação
+    def data_br(data)
+        data.strftime("%d/%m/%Y")
+    end
+
+    def nome_aplicacao
+        "Crypto Wallet App"
+    end
 end
