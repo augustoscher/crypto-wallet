7f4b8a99aece5828fe561d03693c258f08fd3ff4
Augusto Scher <augustoscher@gmail.com>
2019-05-01 15:25:11 -0300

    refatorando task seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,24 +10,28 @@
 
 # arquivo de inicialização de banco de dados.
 
-puts "Inicializando tabela de moedas"
-Coin.create!(
-  [ 
-    {
-      description: "Bitcoin",
-      acronym: "BTC",
-      url_image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTQwvZrzgk9W9LwnL2Le9K42wEnsgM8pd9DhCUz8Wn1EKzFXyKaqA"
-    },
-    {
-      description: "Ethereum",
-      acronym: "ETH",
-      url_image: "https://img2.gratispng.com/20180330/wae/kisspng-ethereum-bitcoin-cryptocurrency-logo-tether-bitcoin-5abdfe01b6f4b4.2459439115224007697494.jpg"
-    },
-    {
-      description: "Dash",
-      acronym: "DASH",
-      url_image: "https://cdn1.iconfinder.com/data/icons/cryptocurrency-round-color/128/blockchain_cryptocurrency_currency_Dash_Darkcoin_XCoin_1-512.png"
-    }
-  ]
-)
-puts "Moedas inicializadas com sucesso"
+spinner = TTY::Spinner.new("[:spinner] Inicializando moedas")
+spinner.auto_spin
+
+coins = [ 
+          {
+            description: "Bitcoin",
+            acronym: "BTC",
+            url_image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTQwvZrzgk9W9LwnL2Le9K42wEnsgM8pd9DhCUz8Wn1EKzFXyKaqA"
+          },
+          {
+            description: "Ethereum",
+            acronym: "ETH",
+            url_image: "https://img2.gratispng.com/20180330/wae/kisspng-ethereum-bitcoin-cryptocurrency-logo-tether-bitcoin-5abdfe01b6f4b4.2459439115224007697494.jpg"
+          },
+          {
+            description: "Dash",
+            acronym: "DASH",
+            url_image: "https://cdn1.iconfinder.com/data/icons/cryptocurrency-round-color/128/blockchain_cryptocurrency_currency_Dash_Darkcoin_XCoin_1-512.png"
+          }
+        ]
+
+coins.each do |coin| 
+  Coin.find_or_create_by!(coin)
+end 
+spinner.success("(Moedas inicializadas com sucesso)")
