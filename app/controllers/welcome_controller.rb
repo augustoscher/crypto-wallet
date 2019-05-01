f17aebcc4eeb9510139441bdc9f921138aebe037
Augusto Scher <augustoscher@gmail.com>
2019-05-01 12:47:06 -0300

    layouts, fluxo mvc, variaveis de instancia, parametros

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,6 @@
 class WelcomeController < ApplicationController
+  layout "adm"
   def index
+    @nome = params[:nome_param]
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,4 +7,14 @@ module ApplicationHelper
     def nome_aplicacao
         "Crypto Wallet App"
     end
+
+    def ambiente_rails
+        if Rails.env.development?
+            "Desenvolvimento"
+        elsif Rails.env.production?
+            "Produção"
+        else
+            "Teste"
+        end
+    end
 end
