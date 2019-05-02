787d310b276ba12da803ba932fa4843d8ece0273
Augusto Scher <augustoscher@gmail.com>
2019-05-02 00:10:12 -0300

    permitir editar e informar uma mining_type para uma coin atraves da view, ajustar os formularios...

--- app/controllers/coins_controller.rb
+++ app/controllers/coins_controller.rb
@@ -1,5 +1,6 @@
 class CoinsController < ApplicationController
   before_action :set_coin, only: [:show, :edit, :update, :destroy]
+  before_action :set_mining_type_options, only: [:create, :new, :edit, :update] 
 
   # GET /coins
   # GET /coins.json
@@ -62,6 +63,10 @@ class CoinsController < ApplicationController
   end
 
   private
+    def set_mining_type_options
+      @mining_type_options = MiningType.all.pluck(:description, :id)
+    end
+  
     # Use callbacks to share common setup or constraints between actions.
     def set_coin
       @coin = Coin.find(params[:id])
@@ -69,6 +74,6 @@ class CoinsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def coin_params
-      params.require(:coin).permit(:description, :acronym, :url_image)
+      params.require(:coin).permit(:description, :acronym, :url_image, :mining_type_id)
     end
 end
