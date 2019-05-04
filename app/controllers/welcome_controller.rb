34af95f9a7dee8df4362a37e91119c07fe49fac2
Augusto Scher <augustoscher@gmail.com>
2019-05-04 17:44:46 -0300

    session and cookies

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,8 @@
 class WelcomeController < ApplicationController
   layout "adm"
   def index
+    cookies[:curso] = "My cookie example"
+    session[:session_example] = "My session example"
     @nome = params[:nome_param]
   end
 end
